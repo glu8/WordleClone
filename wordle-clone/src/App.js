2f804e0dd2af7fe52666f01bebad9b111f29e5fa
geoffreylu8 <jgl3@williams.edu>
2022-02-01 21:51:42 -0500

    basic working prototype

--- wordle-clone/src/App.js
+++ wordle-clone/src/App.js
@@ -1,48 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import WordRow from './components/WordRow.js';
+import Board from './components/Board.js';
+
 
 function App() {
+  
+  // const [count, updateCount] = useState(0);
+  const word = "THICK"
+  const validLetters = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
 
-  // const [currentLetter, updateCurrentLetter] = useState(document.querySelector("div.letter[data-state='idle']"));
 
   useEffect(() => {
+    var count = 0
 
-    if (window !== undefined) {
-        window.addEventListener('keydown', logKey);
+    function logKey(e) {
+      var currentLetter = null;
+      var inUse = null;
+     
+      if ((e.key.toLowerCase() === 'backspace')&&(count > 0)) {
+        inUse = document.querySelectorAll("div.letter[data-state='inUse']")
+        currentLetter = inUse[inUse.length - 1]
+        currentLetter.dataset.state = "idle"
+        currentLetter.innerHTML = " "
+        count -= 1
+      } else if (e.key === "Enter") {
+        inUse = document.querySelectorAll("div.letter[data-state='inUse']");
+
+        for (let i = 0; i < inUse.length; i++) {
+          currentLetter = inUse[i]
+          
+          if (currentLetter.innerHTML === word[i]) {
+            currentLetter.dataset.state = "correct"
+          } else if (word.includes(currentLetter.innerHTML)) {
+            currentLetter.dataset.state = "present"
+          } else {
+            currentLetter.dataset.state = "absent"
+          }
+          
+        }
+        count = 0
+      } else if (count < 5) {
+          if (validLetters.has(e.key)) {
+            currentLetter = document.querySelector("div.letter[data-state='idle']");
+            currentLetter.dataset.state = "inUse";
+            currentLetter.innerHTML = e.key.toUpperCase();
+            count += 1;
+          } 
+      } 
+      
+      // await updateCurrentLetter(document.querySelector("div.letter[data-state='idle']"))
+     
+      return false;
+    }
+
+    if (document !== undefined) {
+        document.addEventListener('keydown', logKey);
     }
     return () => {
-        if (window !== undefined) {
-          window.addEventListener('keydown', logKey);
+        if (document !== undefined) {
+          document.addEventListener('keydown', logKey);
         }
       }
-    }, []) 
-
-  async function logKey(e) {
-
-    var currentLetter = document.querySelector("div.letter[data-state='idle']");
-
-    console.log(e.key)
-    console.log("initial", currentLetter)
-    currentLetter.dataset.state = "inUse"
-    currentLetter.innerHTML = e.key
-    // await updateCurrentLetter(document.querySelector("div.letter[data-state='idle']"))
-    console.log("after", currentLetter)
-    
-    return false;
+}, []) 
 
 
-  }
   
 
   return (
     <div className="App">
-      <WordRow />
-      <WordRow />
-      <WordRow />
-      <WordRow />
-      <WordRow />
-      <WordRow />
+      <Board />
     </div>
   );
 }
--- wordle-clone/src/components/WordRow.js
+++ wordle-clone/src/components/WordRow.js
@@ -3,13 +3,13 @@ import '../App.css';
 function WordRow() {
 
   return (
-      <div className="wordRow">
-      <div className="letter" data-state="idle" data-order="1">0</div>
-      <div className="letter" data-state="idle" data-order="2">0</div>
-      <div className="letter" data-state="idle" data-order="3">0</div>
-      <div className="letter" data-state="idle" data-order="4">0</div>
-      <div className="letter" data-state="idle" data-order="5">0</div>
-      </div>
+      <span className="wordRow">
+      <div className="letter" data-state="idle" data-order="1"> </div>
+      <div className="letter" data-state="idle" data-order="2"> </div>
+      <div className="letter" data-state="idle" data-order="3"> </div>
+      <div className="letter" data-state="idle" data-order="4"> </div>
+      <div className="letter" data-state="idle" data-order="5"> </div>
+      </span>
   );
 }
 
