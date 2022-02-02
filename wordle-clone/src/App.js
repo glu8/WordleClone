f7b2717417f3dfdc5553ed545b6420121b7ceb9e
geoffreylu8 <jgl3@williams.edu>
2022-02-02 18:04:01 -0500

    working game prototype

--- wordle-clone/src/App.js
+++ wordle-clone/src/App.js
@@ -1,74 +1,101 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Board from './components/Board.js';
-
+import WordRow from './components/WordRow.js';
+import { words } from './words.js';
 
 function App() {
-  
-  // const [count, updateCount] = useState(0);
-  const word = "THICK"
-  const validLetters = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
 
+  // const [count, updateCount] = useState(0);
+  const [completed, updateCompleted] = useState(false);
+  const [failed, updateFailed] = useState(false);
+  const [word, updateWord] = useState("");
 
   useEffect(() => {
-    var count = 0
+    updateWord(words[Math.floor(Math.random() * words.length)]);
+  }, [])
+
+  useEffect(() => {
+    const validLetters = new Set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
+    var count = 0;
+    var tries = 0;
 
     function logKey(e) {
       var currentLetter = null;
       var inUse = null;
-     
-      if ((e.key.toLowerCase() === 'backspace')&&(count > 0)) {
-        inUse = document.querySelectorAll("div.letter[data-state='inUse']")
-        currentLetter = inUse[inUse.length - 1]
-        currentLetter.dataset.state = "idle"
-        currentLetter.innerHTML = " "
-        count -= 1
-      } else if (e.key === "Enter") {
-        inUse = document.querySelectorAll("div.letter[data-state='inUse']");
 
+      if ((e.key.toLowerCase() === 'backspace') && (count > 0)) {
+        inUse = document.querySelectorAll("div.letter[data-state='inUse']");
+        currentLetter = inUse[inUse.length - 1];
+        currentLetter.dataset.state = "idle";
+        currentLetter.innerHTML = " ";
+        count -= 1;
+      } else if ((e.key === "Enter") && (count === 5)) {
+        inUse = document.querySelectorAll("div.letter[data-state='inUse']");
+        var correctCount = 0;
         for (let i = 0; i < inUse.length; i++) {
-          currentLetter = inUse[i]
-          
-          if (currentLetter.innerHTML === word[i]) {
-            currentLetter.dataset.state = "correct"
-          } else if (word.includes(currentLetter.innerHTML)) {
-            currentLetter.dataset.state = "present"
+          currentLetter = inUse[i];
+          if (currentLetter.innerHTML.toLowerCase() === word[i]) {
+            currentLetter.dataset.state = "correct";
+            correctCount += 1;
+          } else if (word.includes(currentLetter.innerHTML.toLowerCase())) {
+            currentLetter.dataset.state = "present";
           } else {
-            currentLetter.dataset.state = "absent"
+            currentLetter.dataset.state = "absent";
           }
-          
+
         }
-        count = 0
+
+        tries += 1;
+
+        if (correctCount === 5) {
+          updateCompleted(completed => completed + 1);
+          document.removeEventListener('keydown', logKey);
+        } else if (tries === 6) {
+          updateFailed(failed => failed + 1);
+          document.removeEventListener('keydown', logKey);
+        }
+
+        count = 0;        
       } else if (count < 5) {
-          if (validLetters.has(e.key)) {
-            currentLetter = document.querySelector("div.letter[data-state='idle']");
-            currentLetter.dataset.state = "inUse";
-            currentLetter.innerHTML = e.key.toUpperCase();
-            count += 1;
-          } 
-      } 
-      
-      // await updateCurrentLetter(document.querySelector("div.letter[data-state='idle']"))
-     
+        if (validLetters.has(e.key)) {
+          currentLetter = document.querySelector("div.letter[data-state='idle']");
+          currentLetter.dataset.state = "inUse";
+          currentLetter.innerHTML = e.key.toUpperCase();
+          count += 1;
+        }
+      }
+
+
       return false;
     }
 
     if (document !== undefined) {
-        document.addEventListener('keydown', logKey);
+      document.addEventListener('keydown', logKey);
     }
     return () => {
-        if (document !== undefined) {
-          document.addEventListener('keydown', logKey);
-        }
+      if (document !== undefined) {
+        document.removeEventListener('keydown', logKey);
       }
-}, []) 
+    }
+  }, [word])
 
 
-  
+
 
   return (
     <div className="App">
-      <Board />
+      <div className="header">
+        <h2>Wordle Clone</h2>
+      </div>
+      <WordRow />
+      <WordRow />
+      <WordRow />
+      <WordRow />
+      <WordRow />
+      <WordRow />
+
+      {completed ? <div> Completed! </div> : ""}
+      {failed ? <div> Correct word: {word.toUpperCase()} </div> : ""}
     </div>
   );
 }
